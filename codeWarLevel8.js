5b45afbd3b9c2bcb7153a392b2c4f64e172a3f81
Melissa Smith <melissasmith@Melissas-MacBook-Air.local>
2019-09-28 05:53:01 -0700

    war

--- codeWarLevel8.js
+++ codeWarLevel8.js
@@ -11,3 +11,15 @@ function hoopCount(n) {
 }
 
 hoopCount(10)
+
+// input a number output its equivelent word
+
+function switchItUp(number) {
+    return number === 0 ? 'Zero' : number === 1 ? 'One' : number === 2 ? 'Two' : number === 3 ? 'Three' : number === 4 ? 'Four' : number === 5 ? 'Five' : number === 6 ? 'Six' : number === 7 ? 'Seven' : number === 8 ? 'Eight' : number === 9 ? 'Nine' : null
+}
+
+switchItUp(9)
+
+switchItUp = n => ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][n]
+
+switchItUp(9)
