b1ec55100a85a70b465e90f37603bde8f26388ad
Melissa Smith <melissasmith@Melissas-MacBook-Air.local>
2019-10-04 14:34:22 -0700

    codewar

--- codeWarLevel8.js
+++ codeWarLevel8.js
@@ -23,3 +23,22 @@ switchItUp(9)
 switchItUp = n => ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"][n]
 
 switchItUp(9)
+
+//************* Duplicate letters of a string*/
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+function accum(str) {
+    return str.split('').map((letter, i) =>
+        (letter.toUpperCase() + letter.toLowerCase().repeat(i))).join('_')
+
+}
+
+
+accum('abc')
